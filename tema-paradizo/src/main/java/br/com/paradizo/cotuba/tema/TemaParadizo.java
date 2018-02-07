b3890db9884f3f5a4411f4710c988fe1bb26c977
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-02-07 14:52:06 -0200

    corrigindo mensagem

--- tema-paradizo/src/main/java/br/com/paradizo/cotuba/tema/TemaParadizo.java
+++ tema-paradizo/src/main/java/br/com/paradizo/cotuba/tema/TemaParadizo.java
@@ -9,7 +9,7 @@ import cotuba.plugin.LogoAposRenderizarMDParaHTML;
 public class TemaParadizo implements LogoAposRenderizarMDParaHTML {
 
 	public void trata(Capitulo capitulo) {
-		System.out.println("Executando o tema Caelum para o capítulo: " + capitulo.getTitulo());
+		System.out.println("Executando o tema Paradizo para o capítulo: " + capitulo.getTitulo());
 		String html = capitulo.getConteudoHTML();
 		Document doc = Jsoup.parse(html);
 		doc.select("head").prepend("<style> h1 { border: 1px dashed black; } </style>");
